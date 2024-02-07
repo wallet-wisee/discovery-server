611266270a5fc7465e644c1172e4921dafab8541
gervasioartur <gervasioartur@outlook.com.br>
2024-02-07 03:21:16 -0300

    chore: initial project setup

---
+++ src/test/java/com/walletwise/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.walletwise.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void main() {
+        DiscoveryServerApplication.main(new String[]{});
+    }
+}
