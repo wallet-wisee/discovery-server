14d026e0b3e6134bbfae2464641ac5b6c56428ae
gervasioartur <gervasioartur@outlook.com.br>
2024-02-29 04:53:53 -0300

    chore: configuring zipkin

--- src/main/java/com/walletwise/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/walletwise/discoveryserver/DiscoveryServerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
-        System.out.println("ggggggggggg");
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 
